b90316b4d1a89ae73a3a972de49f02161db3fddc
ronnieboy2k <ronnie.boy2k@gmail.com>
2026-09-08 21:49:59 +0800

    test: add signature form page tests

--- signatures/tests.py
+++ signatures/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class SignatureFormPageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/signature/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("signature_form"))
+        self.assertEqual(response.status_code, 200)
