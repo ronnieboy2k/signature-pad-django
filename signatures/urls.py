d663312fa957c1f8420b80ca56fad73da49f06c1
ronnieboy2k <ronnie.boy2k@gmail.com>
2026-09-10 20:47:55 +0800

    feat: redirect root URL to signature page

--- signatures/urls.py
+++ signatures/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from .views import SignatureFormPageView, SignatureResultPageView
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path(
+        "",
+        RedirectView.as_view(
+            url="/signature/",
+            permanent=False,
+        ),
+        name="home",
+    ),
     path("signature/", SignatureFormPageView.as_view(), name="signature_form"),
     path(
         "signature/result/", SignatureResultPageView.as_view(), name="signature_result"
